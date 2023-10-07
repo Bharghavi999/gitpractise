79871ceae5fc6d1d3d0475c82407d06beaf42daa
Bharghavi23 <57526493+Bharghavi999@users.noreply.github.com>
2023-10-07 10:28:47 +0530

    Update Program1.cs

--- Program1.cs
+++ Program1.cs
@@ -1,1 +1,34 @@
-u
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace gitpractise
+{
+    public class Program1
+    {
+        public static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://www.google.com");
+
+            driver.Manage().Window.Maximize();
+
+            IWebElement search = driver.FindElement(By.Name("q"));
+
+            //IWebElement find = driver.FindElement(By.XPath("//div[@class='wM6W7d']//span[1]"));
+
+            search.SendKeys("Selenium webdriver");
+
+            search.Submit();
+
+            Thread.Sleep(3000);
+
+            driver.Close();
+
+        }
+    }
+
+}
+
+
