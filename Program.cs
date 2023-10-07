2068ae6b9e4576f3c89dd89013a1fb2453e70f3d
Bharghavi999 <bharghavi99rm@gmail.com>
2023-10-07 10:10:08 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace gitpractise
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://www.google.com");
+
+            driver.Manage().Window.Maximize();
+
+            IWebElement search = driver.FindElement(By.Name("q"));
+
+            //IWebElement find = driver.FindElement(By.XPath("//div[@class='wM6W7d']//span[1]"));
+
+            search.SendKeys("Selenium");
+
+            search.Submit();
+
+            Thread.Sleep(3000);
+
+            driver.Close();
+
+        }
+    }
+
+}
+
